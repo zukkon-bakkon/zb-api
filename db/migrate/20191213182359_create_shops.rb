37995473736ce8a7f37c5d20b7f47618b99e0c0d
Tom0430 <53653372+Tom0430@users.noreply.github.com>
2019-12-14 04:42:01 +0900

    create model

---
+++ db/migrate/20191213182359_create_shops.rb
@@ -0,0 +1,22 @@
+class CreateShops < ActiveRecord::Migration[6.0]
+  def change
+    create_table :shops do |t|
+      t.string :name
+      t.string :shop_url
+      t.time :open_time
+      t.time :close_time
+      t.time :play_time
+      t.integer :price
+      t.integer :tell_number
+      t.string :description
+      t.string :trip_area
+      t.string :holiday
+      t.integer :admission
+      t.integer :designation_fee
+      t.integer :trans_fee
+      t.string :payment
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/actress_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ActressTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
