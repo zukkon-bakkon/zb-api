225849300da676e16475d8dde2f9d74735b85228
s14228so <saongtx7@gmail.com>
2019-12-15 16:27:52 +0900

    ok

--- app/controllers/scrapings_controller.rb
+++ app/controllers/scrapings_controller.rb
@@ -19,20 +19,11 @@ class ScrapingsController < ApplicationController
       driver.manage.timeouts.implicit_wait = @timeout
       wait = Selenium::WebDriver::Wait.new(timeout: @wait_time)
 
-      # Yahooを開く
       driver.get("https://www.dto.jp/shibuya/shop-list")
 
       # ちゃんと開けているか確認するため、sleepを入れる
       sleep 2
 
-
-
-      #**
-        # ブラウザでさせたい動作を記載する
-
-        # ex. 検索欄に'Ruby'と入力して、検索ボタンを押す処理
-
-        # 検索欄/検索ボタン取得
         begin
 
               shop_names = driver.find_elements(:css, '.com_shop_list.style1 .shop > a') 
@@ -68,10 +59,6 @@ class ScrapingsController < ApplicationController
         rescue Selenium::WebDriver::Error::NoSuchElementError
           p 'no such element error!!'
         end
-
-      #   search_box.send_keys 'Ruby'
-      #   search_btn.click
-
 
       # ドライバーを閉じる
       driver.quit
