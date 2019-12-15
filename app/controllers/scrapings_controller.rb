ccf4529b46dfd3f0649c3b2562d6938e8ad91573
s14228so <saongtx7@gmail.com>
2019-12-15 15:33:30 +0900

    ok

--- app/controllers/scrapings_controller.rb
+++ app/controllers/scrapings_controller.rb
@@ -16,7 +16,7 @@ driver.manage.timeouts.implicit_wait = @timeout
 wait = Selenium::WebDriver::Wait.new(timeout: @wait_time)
 
 # Yahooを開く
-driver.get("https://www.dto.jp/shop/21530")
+driver.get("https://www.dto.jp/shibuya/shop-list")
 
 # ちゃんと開けているか確認するため、sleepを入れる
 sleep 2
@@ -29,10 +29,10 @@ sleep 2
   # 検索欄/検索ボタン取得
   begin
 
-        # shop_names = driver.find_elements(:css, '.shop > a') 
-        # shops = shop_names.map {|name| {name: name.text,url: name.attribute("href")}}
-        # shops.each do |shop|
-        #  driver.get(shop[:url])
+        shop_names = driver.find_elements(:css, '.shop > a') 
+        shops = shop_names.map {|name| {name: name.text,url: name.attribute("href")}}
+        shops.each do |shop|
+         driver.get(shop[:url])
          name = driver.find_element(:css, '.com_body > h2') 
          table_elements = driver.find_elements(:css, 'div.com_td.style2') 
          trip_area = driver.find_element(:css, 'div.com_td.style3') 
@@ -56,7 +56,7 @@ sleep 2
          shop_one = Shop.new(name: name.text, image_url: image_url,tell_number: table_elements[0].text,trip_area: trip_area.text, holiday: table_elements[3].text,open_time: table_elements[5].text, title: title.text, description: description, admission:  fees[0].text == "無料"  ?  0 : fees[0].text.delete("円").delete(",").to_i, designation_fee: fees[1].text == "無料" ? 0 :fees[1].text.delete("円").delete(",").to_i )
          shop_one.save
         #  Girl.create(shop_id: shop_one.id)
-        # end
+        end
 
    
   rescue Selenium::WebDriver::Error::NoSuchElementError
